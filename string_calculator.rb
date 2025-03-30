979225927591457b9b69b6091fd4fdf4bd9d4a4e
rajramk12 <raj.ramk12@outlook.com>
2025-03-30 19:14:39 +0530

    v.05 with delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,16 @@ class StringCalculator
 		out = 0 
 		return out if nums.size < 1
 
-		nums = nums.split(/[\n,]/)
+		delimiters = ["\n", ","]
+		
+		if nums[0..1] == "//"
+			delimiters << nums[2]
+			delimiters << nums[0..1]
+		end
+
+		pattern = Regexp.new(delimiters.to_s)
+
+		nums = nums.split(pattern).reject(&:empty?)
 
 		nums.each do |num|
 			out += Integer(num)
