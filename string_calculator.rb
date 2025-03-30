5dc20d59e25de0d1d253f8fa8e1e99c29d4bd638
rajramk12 <raj.ramk12@outlook.com>
2025-03-30 18:06:06 +0530

    v0.1

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,12 @@
 class StringCalculator
+	def add(*args)
+		out = 0 
+		return out if args.size < 1
+
+		args.each do |num|
+			out += num
+		end
+		
+		return out
+	end
 end
--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -2,9 +2,14 @@ require 'minitest\autorun'
 require_relative 'string_calculator'
 class StringTestCalculator < Minitest::Test
 	def setup
-		@calc = Calculator.new
+		@calc = StringCalculator.new
 	end
 
-	def test_add_exists
+	def test_add_blank
+		assert_equal  0, @calc.add()
+	end
+
+	def test_add_numbers
+		assert_equal 3, @calc.add(1,2)
 	end
 end
